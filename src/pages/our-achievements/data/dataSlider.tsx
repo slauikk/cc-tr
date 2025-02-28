60fd87f20d53119d9f492307585c25cdd1495ce0
slauikk <zzzyaroslav63@gmail.com>
2025-02-28 02:09:29 +0200

    feat: add content

---
+++ src/pages/our-achievements/data/dataSlider.tsx
@@ -0,0 +1,30 @@
+import {ReactNode} from "react"
+
+import {SlideItem} from "../ui/slide-item"
+
+export interface ISponsors {
+  img: string
+}
+
+export const dataSponsorsSource_1: ISponsors[][] = [
+  [
+    {img: "/assets/achivements/10.jpg"},
+    {img: "/assets/achivements/11.jpg"},
+    {img: "/assets/achivements/12.jpg"},
+    {img: "/assets/achivements/13.jpg"},
+    {img: "/assets/achivements/14.png"},
+  ],
+]
+
+export let dataSponsors_1: ReactNode[][] = [[]]
+
+Array(dataSponsorsSource_1.length)
+  .fill(null)
+  .map(() => (dataSponsors_1 = [...dataSponsors_1, []]))
+
+dataSponsorsSource_1.map((e, i) => {
+  e.map((el) => {
+    const elem = <SlideItem {...el} />
+    dataSponsors_1[i] = [...dataSponsors_1[i], elem]
+  })
+})
