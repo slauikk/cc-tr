b9a22281f7a4502569b162b61747e287c1367410
slauikk <zzzyaroslav63@gmail.com>
2025-03-17 11:39:12 +0200

    feat: add content

--- src/pages/syllabuses/lib/dataContent.tsx
+++ src/pages/syllabuses/lib/dataContent.tsx
@@ -2,78 +2,301 @@ export const dataContent = [
   {
     content: (
       <>
-          <li><a href="https://eguru1.tk.te.ua/enrol/index.php?id=24">Історія та культура України Вахрів
-              Ірина
-              Ігорівна</a></li>
-          <li><a href="https://eguru1.tk.te.ua/course/view.php?id=1141">Історія та культура України Черниш
-              Наталія Андріївна</a></li>
-          <li><a href="https://eguru1.tk.te.ua/enrol/index.php?id=373">Філософія</a></li>
-          <li><a href="https://eguru1.tk.te.ua/enrol/index.php?id=893">Економічна теорія</a></li>
-          <li><a href="https://eguru1.tk.te.ua/enrol/index.php?id=75">Основи правознавства</a></li>
-          <li><a href="https://eguru1.tk.te.ua/enrol/index.php?id=698">Українська мова (за професійним спрямуванням)</a>
-          </li>
-          <li><a href="https://eguru1.tk.te.ua/course/view.php?id=1188">Іноземна мова (за професійним спрямуванням)
-              Катола
-              Антоніна Іванівна</a>
-          </li>
-          <li><a href="https://eguru1.tk.te.ua/course/view.php?id=203">Іноземна мова (за професійним спрямуванням)
-              Олендер Катерина Петрівна</a>
-          </li>
-          <li><a href="https://eguru1.tk.te.ua/course/view.php?id=1015">Фізичне виховання</a></li>
-          <li><a href="https://eguru1.tk.te.ua/course/view.php?id=1244">Вища математика Фігурська Любов Володимирівна
-          </a></li>
-          <li><a href="https://eguru1.tk.te.ua/enrol/index.php?id=406">Вища математика Погорілець Анна Володимирівна</a>
-          </li>
-          <li><a href="https://eguru1.tk.te.ua/course/view.php?id=286">Фізика Алексевич Наталія Степанівна</a></li>
-          <li><a href="https://eguru1.tk.te.ua/course/view.php?id=1223">Фізика Олійник Дмитро Васильович</a></li>
-          <li><a href="https://eguru1.tk.te.ua/course/view.php?id=214">Інформатика</a></li>
-          <li><a href="https://eguru1.tk.te.ua/course/view.php?id=662">Основи теорії кіл</a></li>
-          <li><a href="https://eguru1.tk.te.ua/course/view.php?id=600">Інженерна та комп'ютерна графіка</a></li>
-          <li><a href="https://eguru1.tk.te.ua/course/view.php?id=309">Економіка промисловості</a></li>
-          <li><a href="https://eguru1.tk.te.ua/course/view.php?id=753">Основи підприємництва</a></li>
-          <li><a href="https://eguru1.tk.te.ua/course/view.php?id=595">Основи екології</a></li>
-          <li><a href="https://eguru1.tk.te.ua/course/view.php?id=1136">Безпека життєдіяльності</a></li>
-          <li><a href="https://eguru1.tk.te.ua/course/view.php?id=1325">Основи охорони праці</a></li>
-          <li><a href="https://eguru1.tk.te.ua/enrol/index.php?id=803">Введення в спеціальність</a></li>
-          <li><a href="https://eguru1.tk.te.ua/course/view.php?id=563">Сигнали та процеси в радіотехніці</a></li>
-          <li><a href="https://eguru1.tk.te.ua/enrol/index.php?id=970">Електронні пристрої та мікроелектроніка Приймак Віктор Адамович
-          </a></li>
-          <li><a href="https://eguru1.tk.te.ua/course/view.php?id=408">Електронні пристрої та мікроелектроніка Недошитко
-              Людмила Миколаївна</a></li>
-          <li><a href="https://eguru1.tk.te.ua/course/view.php?id=867">Основи метрології, стандартизації і
-              сертифікації</a></li>
-          <li><a href="https://eguru1.tk.te.ua/course/view.php?id=257">Електровимірювання в телекомунікаційних
-              системах</a></li>
-          <li><a href="https://eguru1.tk.te.ua/course/view.php?id=626">Схемотехніка аналогових пристроїв</a></li>
-          <li><a href="https://eguru1.tk.te.ua/course/view.php?id=399">Схемотехніка цифрових пристроїв</a></li>
-          <li><a href="https://eguru1.tk.te.ua/course/view.php?id=1174">Автоматика та робототехніка</a></li>
-          <li><a href="https://eguru1.tk.te.ua/course/view.php?id=400">Мікропроцесорні пристрої та системи</a></li>
-          <li><a href="https://eguru1.tk.te.ua/course/view.php?id=394">Системи автоматизованого проектування</a></li>
-          <li><a href="https://eguru1.tk.te.ua/course/view.php?id=418">Програмування мікроконтролерів</a></li>
-          <li><a href="https://eguru1.tk.te.ua/course/view.php?id=258">Системи електроживлення</a></li>
-          <li><a href="https://eguru1.tk.te.ua/course/view.php?id=88">Радіопередавальні пристрої</a></li>
-          <li><a href="https://eguru1.tk.te.ua/course/view.php?id=87">Радіоприймальні пристрої</a></li>
-          <li><a href="https://eguru1.tk.te.ua/course/view.php?id=486">Телекомунікаційні пристрої приймання і
-              передавання
-              інформації</a></li>
-          <li><a href="https://eguru1.tk.te.ua/course/view.php?id=863">Акустичні пристрої</a></li>
-          <li><a href="https://eguru1.tk.te.ua/course/view.php?id=419">Основи телебачення та телевізійні системи</a>
-          </li>
-          <li><a href="https://eguru1.tk.te.ua/course/view.php?id=401">Радіотехнічні системи</a></li>
-          <li><a href="https://eguru1.tk.te.ua/course/view.php?id=664">Основи діагностики, обслуговування та ремонту
-              телекомунікаційних систем і мереж</a></li>
-          <li><a href="https://eguru1.tk.te.ua/course/view.php?id=388">Конструювання РЕА</a></li>
-          <li><a href="https://eguru1.tk.te.ua/course/view.php?id=387">Технології виробництва РЕА</a></li>
-          <li><a href="https://eguru1.tk.te.ua/course/view.php?id=606">Матеріали і компоненти радіотехнічних
-              систем (Матеріали і компоненти електронних систем)</a>
-          </li>
-          <li><a href="https://eguru1.tk.te.ua/course/view.php?id=257">Електровимірювання в телекомунікаційних системах (Електровимірювання)</a></li>
-          <li>
-              <a href="https://eguru1.tk.te.ua/enrol/index.php?id=664"> Основи діагностики, ремонту і регулювання РЕА</a>
-          </li>
-          <li>
-              <a href="https://eguru1.tk.te.ua/enrol/index.php?id=1331">Основи телебачення </a>
-          </li>
+        <li>
+          Історія та культура України
+          <ul>
+            <li>
+              <a href="https://eguru1.tk.te.ua/enrol/index.php?id=24">Покликання 1</a>
+            </li>
+            <li>
+              <a href="https://eguru1.tk.te.ua/course/view.php?id=1141">Покликання 2</a>
+            </li>
+          </ul>
+        </li>
+        {/*<li>*/}
+        {/*  <a href="https://eguru1.tk.te.ua/enrol/index.php?id=24">*/}
+        {/*    Історія та культура України Вахрів Ірина Ігорівна*/}
+        {/*  </a>*/}
+        {/*</li>*/}
+        {/*<li>*/}
+        {/*  <a href="https://eguru1.tk.te.ua/course/view.php?id=1141">*/}
+        {/*    Історія та культура України Черниш Наталія Андріївна*/}
+        {/*  </a>*/}
+        {/*</li>*/}
+        <li>
+          <a href="https://eguru1.tk.te.ua/enrol/index.php?id=373">Філософія</a>
+        </li>
+        <li>
+          <a href="https://eguru1.tk.te.ua/enrol/index.php?id=893">
+            Економічна теорія
+          </a>
+        </li>
+        <li>
+          <a href="https://eguru1.tk.te.ua/enrol/index.php?id=75">
+            Основи правознавства
+          </a>
+        </li>
+        <li>
+          <a href="https://eguru1.tk.te.ua/enrol/index.php?id=698">
+            Українська мова (за професійним спрямуванням)
+          </a>
+        </li>
+        <li>
+          Іноземна мова (за професійним спрямуванням)
+          <ul>
+            <li>
+              <a href="https://eguru1.tk.te.ua/course/view.php?id=1188">Покликання 1</a>
+            </li>
+            <li>
+              <a href="https://eguru1.tk.te.ua/course/view.php?id=203">Покликання 2</a>
+            </li>
+          </ul>
+        </li>
+        {/*<li>*/}
+        {/*  <a href="https://eguru1.tk.te.ua/course/view.php?id=1188">*/}
+        {/*    Іноземна мова (за професійним спрямуванням) Катола Антоніна Іванівна*/}
+        {/*  </a>*/}
+        {/*</li>*/}
+        {/*<li>*/}
+        {/*  <a href="https://eguru1.tk.te.ua/course/view.php?id=203">*/}
+        {/*    Іноземна мова (за професійним спрямуванням) Олендер Катерина*/}
+        {/*    Петрівна*/}
+        {/*  </a>*/}
+        {/*</li>*/}
+        <li>
+          <a href="https://eguru1.tk.te.ua/course/view.php?id=1015">
+            Фізичне виховання
+          </a>
+        </li>
+        <li>
+          Вища математика
+          <ul>
+            <li>
+              <a href="https://eguru1.tk.te.ua/course/view.php?id=1244">Покликання 1</a>
+            </li>
+            <li>
+              <a href="https://eguru1.tk.te.ua/enrol/index.php?id=406">Покликання 2</a>
+            </li>
+          </ul>
+        </li>
+        {/*<li>*/}
+        {/*  <a href="https://eguru1.tk.te.ua/course/view.php?id=1244">*/}
+        {/*    Вища математика Фігурська Любов Володимирівна*/}
+        {/*  </a>*/}
+        {/*</li>*/}
+        {/*<li>*/}
+        {/*  <a href="https://eguru1.tk.te.ua/enrol/index.php?id=406">*/}
+        {/*    Вища математика Погорілець Анна Володимирівна*/}
+        {/*  </a>*/}
+        {/*</li>*/}
+
+        <li>
+          Фізика
+          <ul>
+            <li>
+              <a href="https://eguru1.tk.te.ua/course/view.php?id=286">Покликання 1</a>
+            </li>
+            <li>
+              <a href="https://eguru1.tk.te.ua/course/view.php?id=1223">Покликання 2</a>
+            </li>
+          </ul>
+        </li>
+        {/*<li>*/}
+        {/*  <a href="https://eguru1.tk.te.ua/course/view.php?id=286">*/}
+        {/*    Фізика Алексевич Наталія Степанівна*/}
+        {/*  </a>*/}
+        {/*</li>*/}
+        {/*<li>*/}
+        {/*  <a href="https://eguru1.tk.te.ua/course/view.php?id=1223">*/}
+        {/*    Фізика Олійник Дмитро Васильович*/}
+        {/*  </a>*/}
+        {/*</li>*/}
+        <li>
+          <a href="https://eguru1.tk.te.ua/course/view.php?id=214">
+            Інформатика
+          </a>
+        </li>
+        <li>
+          <a href="https://eguru1.tk.te.ua/course/view.php?id=662">
+            Основи теорії кіл
+          </a>
+        </li>
+        <li>
+          <a href="https://eguru1.tk.te.ua/course/view.php?id=600">
+            Інженерна та комп'ютерна графіка
+          </a>
+        </li>
+        <li>
+          <a href="https://eguru1.tk.te.ua/course/view.php?id=309">
+            Економіка промисловості
+          </a>
+        </li>
+        <li>
+          <a href="https://eguru1.tk.te.ua/course/view.php?id=753">
+            Основи підприємництва
+          </a>
+        </li>
+        <li>
+          <a href="https://eguru1.tk.te.ua/course/view.php?id=595">
+            Основи екології
+          </a>
+        </li>
+        <li>
+          <a href="https://eguru1.tk.te.ua/course/view.php?id=1136">
+            Безпека життєдіяльності
+          </a>
+        </li>
+        <li>
+          <a href="https://eguru1.tk.te.ua/course/view.php?id=1325">
+            Основи охорони праці
+          </a>
+        </li>
+        <li>
+          <a href="https://eguru1.tk.te.ua/enrol/index.php?id=803">
+            Введення в спеціальність
+          </a>
+        </li>
+        <li>
+          <a href="https://eguru1.tk.te.ua/course/view.php?id=563">
+            Сигнали та процеси в радіотехніці
+          </a>
+        </li>
+        <li>
+          Електронні пристрої та мікроелектроніка
+          <ul>
+            <li>
+              <a href="https://eguru1.tk.te.ua/enrol/index.php?id=970">Покликання 1</a>
+            </li>
+            <li>
+              <a href="https://eguru1.tk.te.ua/course/view.php?id=408">Покликання 2</a>
+            </li>
+          </ul>
+        </li>
+
+        {/*<li>*/}
+        {/*  <a href="https://eguru1.tk.te.ua/enrol/index.php?id=970">*/}
+        {/*    Електронні пристрої та мікроелектроніка Приймак Віктор Адамович*/}
+        {/*  </a>*/}
+        {/*</li>*/}
+        {/*<li>*/}
+        {/*  <a href="https://eguru1.tk.te.ua/course/view.php?id=408">*/}
+        {/*    Електронні пристрої та мікроелектроніка Недошитко Людмила Миколаївна*/}
+        {/*  </a>*/}
+        {/*</li>*/}
+        <li>
+          <a href="https://eguru1.tk.te.ua/course/view.php?id=867">
+            Основи метрології, стандартизації і сертифікації
+          </a>
+        </li>
+        <li>
+          <a href="https://eguru1.tk.te.ua/course/view.php?id=257">
+            Електровимірювання в телекомунікаційних системах
+          </a>
+        </li>
+        <li>
+          <a href="https://eguru1.tk.te.ua/course/view.php?id=626">
+            Схемотехніка аналогових пристроїв
+          </a>
+        </li>
+        <li>
+          <a href="https://eguru1.tk.te.ua/course/view.php?id=399">
+            Схемотехніка цифрових пристроїв
+          </a>
+        </li>
+        <li>
+          <a href="https://eguru1.tk.te.ua/course/view.php?id=1174">
+            Автоматика та робототехніка
+          </a>
+        </li>
+        <li>
+          <a href="https://eguru1.tk.te.ua/course/view.php?id=400">
+            Мікропроцесорні пристрої та системи
+          </a>
+        </li>
+        <li>
+          <a href="https://eguru1.tk.te.ua/course/view.php?id=394">
+            Системи автоматизованого проектування
+          </a>
+        </li>
+        <li>
+          <a href="https://eguru1.tk.te.ua/course/view.php?id=418">
+            Програмування мікроконтролерів
+          </a>
+        </li>
+        <li>
+          <a href="https://eguru1.tk.te.ua/course/view.php?id=258">
+            Системи електроживлення
+          </a>
+        </li>
+        <li>
+          <a href="https://eguru1.tk.te.ua/course/view.php?id=88">
+            Радіопередавальні пристрої
+          </a>
+        </li>
+        <li>
+          <a href="https://eguru1.tk.te.ua/course/view.php?id=87">
+            Радіоприймальні пристрої
+          </a>
+        </li>
+        <li>
+          <a href="https://eguru1.tk.te.ua/course/view.php?id=486">
+            Телекомунікаційні пристрої приймання і передавання інформації
+          </a>
+        </li>
+        <li>
+          <a href="https://eguru1.tk.te.ua/course/view.php?id=863">
+            Акустичні пристрої
+          </a>
+        </li>
+        <li>
+          <a href="https://eguru1.tk.te.ua/course/view.php?id=419">
+            Основи телебачення та телевізійні системи
+          </a>
+        </li>
+        <li>
+          <a href="https://eguru1.tk.te.ua/course/view.php?id=401">
+            Радіотехнічні системи
+          </a>
+        </li>
+        <li>
+          <a href="https://eguru1.tk.te.ua/course/view.php?id=664">
+            Основи діагностики, обслуговування та ремонту телекомунікаційних
+            систем і мереж
+          </a>
+        </li>
+        <li>
+          <a href="https://eguru1.tk.te.ua/course/view.php?id=388">
+            Конструювання РЕА
+          </a>
+        </li>
+        <li>
+          <a href="https://eguru1.tk.te.ua/course/view.php?id=387">
+            Технології виробництва РЕА
+          </a>
+        </li>
+        <li>
+          <a href="https://eguru1.tk.te.ua/course/view.php?id=606">
+            Матеріали і компоненти радіотехнічних систем (Матеріали і компоненти
+            електронних систем)
+          </a>
+        </li>
+        <li>
+          <a href="https://eguru1.tk.te.ua/course/view.php?id=257">
+            Електровимірювання в телекомунікаційних системах
+            (Електровимірювання)
+          </a>
+        </li>
+        <li>
+          <a href="https://eguru1.tk.te.ua/enrol/index.php?id=664">
+            {" "}
+            Основи діагностики, ремонту і регулювання РЕА
+          </a>
+        </li>
+        <li>
+          <a href="https://eguru1.tk.te.ua/enrol/index.php?id=1331">
+            Основи телебачення{" "}
+          </a>
+        </li>
       </>
     ),
   },
