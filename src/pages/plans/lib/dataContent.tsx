fc3ff6ee2725bc17c7d24c15ec05f94c1e24e43a
slauikk <zzzyaroslav63@gmail.com>
2025-05-19 20:35:19 +0300

    feat: add content

--- src/pages/plans/lib/dataContent.tsx
+++ src/pages/plans/lib/dataContent.tsx
@@ -3,22 +3,22 @@ export const dataContent = [
     content: (
       <>
         <li>
-          <a href="public/assets/plans/ТР_БЗСО_2020.pdf">
+          <a href="/assets/plans/ТР_БЗСО_2020.pdf">
             ТР_БЗСО_2020
           </a>
         </li>
         <li>
-          <a href="public/assets/plans/ТР_БЗСО_2022.pdf">
+          <a href="/assets/plans/ТР_БЗСО_2022.pdf">
             ТР_БЗСО_2022
           </a>
         </li>
         <li>
-          <a href="public/assets/plans/ТР_БЗСО_2023.pdf">
+          <a href="/assets/plans/ТР_БЗСО_2023.pdf">
             ТР_БЗСО_2023
           </a>
         </li>
         <li>
-          <a href="public/assets/plans/ТР_ПТО_2023.pdf">
+          <a href="/assets/plans/ТР_ПТО_2023.pdf">
             ТР_ПТО_2023
           </a>
         </li>
